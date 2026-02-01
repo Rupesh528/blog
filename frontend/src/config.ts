93012ff1f05169a0d9d197acc63d3e7cb0cb054f
Rupesh528 <sahanirupesh528@gmail.com>
2026-02-01 14:19:34 +0530

    fix: added env for frontend

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,6 @@
-export const BACKEND_URL = "http://localhost:3000"
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:4000";
