223c31937bb5a3840bc1187e13ee1419b3d5a14e
Rupesh528 <sahanirupesh528@gmail.com>
2026-02-04 13:49:13 +0530

    fixed footer texts

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -4,7 +4,7 @@ export const Quote = () => {
             <div className="max-w-xl mx-auto px-1 py-1">
                 <div className="text-center">
                     <p className="text-xl text-gray-700 mb-2">
-                        "This is a simpleweb app that I made to practice full stack dev"
+                        "This is a simpleweb app that I made to learn dev :)
                     </p>
                     <p className="text-gray-600">
                         - Rupesh Sahani
